91dafacd930155cba6d4ccdc70027e2ce72c88d3
Arnaud Bailly <arnaud.bailly@symbiont.io>
2019-12-06 16:25:08 +0100

    day6/part2

--- day6/Day6.hs
+++ day6/Day6.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Algebra.Graph.Relation
-import Data.Set
-import Data.Text as T
-import Data.Text.Encoding
-import Data.ByteString as BS
-import Data.Tuple
-import System.Environment
-import Control.Arrow
+import           Algebra.Graph.Relation
+import           Control.Arrow
+import qualified Data.ByteString        as BS
+import           Data.Sequence          (Seq ((:<|)), (><))
+import qualified Data.Sequence          as S
+import           Data.Set               hiding (foldl)
+import           Data.Text              (Text)
+import qualified Data.Text              as T
+import           Data.Text.Encoding
+import           Data.Tuple
+import           System.Environment
 
 type Orbit = (Text,Text)
 
@@ -18,7 +21,7 @@ parseOrbit = swap . second T.tail . T.breakOn ")"
 allOrbits :: [(Text,Text)] -> Orbits
 allOrbits = transitiveClosure . edges
 
-sample :: [ T.Text ]
+sample :: [ Text ]
 sample =
  [ "COM)B"
  , "B)C"
@@ -37,18 +40,40 @@ sample =
 
 sample_orbits = allOrbits $ fmap parseOrbit sample
 
-shortest_path :: Orbits -> Text -> Text -> (Int, [Text])
-shortest_path orbits from to = (0, [])
+newtype Path = Path [ Text ]
+  deriving (Eq, Show)
 
-minimum_transfers :: Orbits -> Int
+instance Ord Path where
+  Path p1 `compare` Path p2 =
+    case compare (length p1) (length p2) of
+      LT -> LT
+      GT -> GT
+      EQ -> compare p1 p2
+
+bfs :: Orbits -> Text -> S.Seq Path -> Set Text -> [Text]
+bfs orbits to S.Empty _ = []
+bfs orbits to (Path p@(h : t) :<| rest) visited =
+  if h == to
+  then reverse p
+  else bfs orbits to (rest >< neighbours p) (h `insert` visited)
+
+  where
+    neighbours p@(h : t) =
+      foldl (\ ps x -> Path (x:p) :<| ps) S.empty (postSet h orbits \\ visited)
+
+shortest_path :: Orbits -> Text -> Text -> [Text]
+shortest_path orbits from to = bfs orbits to (S.singleton $ Path [from]) mempty
+
+minimum_transfers :: Orbits -> (Int, [Text])
 minimum_transfers orbits =
   let from = elemAt 0 $ postSet "YOU" orbits
       to = elemAt 0 $ postSet "SAN" orbits
-      refl = reflexiveClosure orbits
-  in fst (shortest_path refl from to)
+      refl = symmetricClosure orbits
+      path = shortest_path refl from to
+  in (length path - 1, path)
 
 main :: IO ()
 main = do
   [input] <- getArgs
-  orbits <- allOrbits . fmap parseOrbit . T.lines . decodeUtf8 <$> BS.readFile input
-  BS.putStr $ (encodeUtf8 $ T.pack $ show $ edgeCount orbits) <> "\n"
+  orbits <- edges . fmap parseOrbit . T.lines . decodeUtf8 <$> BS.readFile input
+  BS.putStr $ encodeUtf8 (T.pack $ show $ minimum_transfers orbits) <> "\n"
