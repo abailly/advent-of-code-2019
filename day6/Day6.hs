bde5e8ca63207f9dd5c7ab0cb8615dadb8af2bbf
Arnaud Bailly <arnaud.oqube@gmail.com>
2019-12-06 08:34:25 +0100

    starting day6/part2

--- day6/Day6.hs
+++ day6/Day6.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Algebra.Graph.Relation
+import Data.Set
 import Data.Text as T
 import Data.Text.Encoding
 import Data.ByteString as BS
@@ -9,10 +10,12 @@ import Control.Arrow
 
 type Orbit = (Text,Text)
 
+type Orbits = Relation Text
+
 parseOrbit :: Text -> (Text, Text)
 parseOrbit = swap . second T.tail . T.breakOn ")"
 
-allOrbits :: [(Text,Text)] -> Relation Text
+allOrbits :: [(Text,Text)] -> Orbits
 allOrbits = transitiveClosure . edges
 
 sample :: [ T.Text ]
@@ -28,7 +31,21 @@ sample =
  , "E)J"
  , "J)K"
  , "K)L"
+ , "K)YOU"
+ , "I)SAN"
  ]
+
+sample_orbits = allOrbits $ fmap parseOrbit sample
+
+shortest_path :: Orbits -> Text -> Text -> (Int, [Text])
+shortest_path orbits from to = (0, [])
+
+minimum_transfers :: Orbits -> Int
+minimum_transfers orbits =
+  let from = elemAt 0 $ postSet "YOU" orbits
+      to = elemAt 0 $ postSet "SAN" orbits
+      refl = reflexiveClosure orbits
+  in fst (shortest_path refl from to)
 
 main :: IO ()
 main = do
