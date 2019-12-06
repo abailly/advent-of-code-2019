697b6f34e7c5685af6621566f288e6df346a202e
Arnaud Bailly <arnaud.bailly@symbiont.io>
2019-12-06 16:32:56 +0100

    some cleanup

--- day6/Day6.hs
+++ day6/Day6.hs
@@ -11,15 +11,49 @@ import           Data.Text.Encoding
 import           Data.Tuple
 import           System.Environment
 
+-- * Basic Types
 type Orbit = (Text,Text)
 
 type Orbits = Relation Text
 
+newtype Path = Path [ Text ]
+  deriving (Eq, Show)
+
+instance Ord Path where
+  Path p1 `compare` Path p2 =
+    case compare (length p1) (length p2) of
+      LT -> LT
+      GT -> GT
+      EQ -> compare p1 p2
+
+-- | Breadth-first traversal of the `Orbits` graph until we reach
+-- `to` point
+bfs :: Orbits -> Text -> S.Seq Path -> Set Text -> [Text]
+bfs orbits to S.Empty _ = []
+bfs orbits to (Path p@(h : t) :<| rest) visited
+  | h == to = p
+  | otherwise = bfs orbits to (rest >< neighbours p) (h `insert` visited)
+
+  where
+    neighbours p@(h : t) =
+      foldl (\ ps x -> Path (x:p) :<| ps) S.empty (postSet h orbits \\ visited)
+
+minimumTransfers :: Orbits -> (Int, [Text])
+minimumTransfers orbits =
+  let from = elemAt 0 $ postSet "YOU" orbits
+      to = elemAt 0 $ postSet "SAN" orbits
+      refl = symmetricClosure orbits
+      path = bfs orbits to (S.singleton $ Path [from]) mempty
+  in (length path - 1, path)
+
+-- * Parsing Orbits file
 parseOrbit :: Text -> (Text, Text)
 parseOrbit = swap . second T.tail . T.breakOn ")"
 
 allOrbits :: [(Text,Text)] -> Orbits
 allOrbits = transitiveClosure . edges
+
+-- * Samples
 
 sample :: [ Text ]
 sample =
@@ -38,42 +72,11 @@ sample =
  , "I)SAN"
  ]
 
-sample_orbits = allOrbits $ fmap parseOrbit sample
+sampleOrbits = allOrbits $ fmap parseOrbit sample
 
-newtype Path = Path [ Text ]
-  deriving (Eq, Show)
-
-instance Ord Path where
-  Path p1 `compare` Path p2 =
-    case compare (length p1) (length p2) of
-      LT -> LT
-      GT -> GT
-      EQ -> compare p1 p2
-
-bfs :: Orbits -> Text -> S.Seq Path -> Set Text -> [Text]
-bfs orbits to S.Empty _ = []
-bfs orbits to (Path p@(h : t) :<| rest) visited =
-  if h == to
-  then reverse p
-  else bfs orbits to (rest >< neighbours p) (h `insert` visited)
-
-  where
-    neighbours p@(h : t) =
-      foldl (\ ps x -> Path (x:p) :<| ps) S.empty (postSet h orbits \\ visited)
-
-shortest_path :: Orbits -> Text -> Text -> [Text]
-shortest_path orbits from to = bfs orbits to (S.singleton $ Path [from]) mempty
-
-minimum_transfers :: Orbits -> (Int, [Text])
-minimum_transfers orbits =
-  let from = elemAt 0 $ postSet "YOU" orbits
-      to = elemAt 0 $ postSet "SAN" orbits
-      refl = symmetricClosure orbits
-      path = shortest_path refl from to
-  in (length path - 1, path)
 
 main :: IO ()
 main = do
   [input] <- getArgs
   orbits <- edges . fmap parseOrbit . T.lines . decodeUtf8 <$> BS.readFile input
-  BS.putStr $ encodeUtf8 (T.pack $ show $ minimum_transfers orbits) <> "\n"
+  BS.putStr $ encodeUtf8 (T.pack $ show $ minimumTransfers orbits) <> "\n"
